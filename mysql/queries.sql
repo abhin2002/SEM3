4dbef09eecf4df9bcb5508631a8e9d2d057f7aec
abhin2002 <abhinajith40@gmail.com>
2022-11-30 15:50:23 +0530

    cycle 1

--- mysql/queries.sql
+++ mysql/queries.sql
@@ -1,9 +1,4 @@
-create database company;
-use company;
-create table employee(SSN varchar(20),Name char(30),
-Address varchar(60),sex char(10),salary int(90),superSSN varchar(20)
-,DNo varchar(20));
-desc employee;
+
 create user 'user1'@'localhost'
 identified by 'password';
 grant select on company.employee to 'user1'@'localhost';
